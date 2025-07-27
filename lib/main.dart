db38d20149a0a191e0085576ebd044b2f0f4a3fa
František Bujnovský <frantisek.bujnovsky@outlook.com>
2025-07-27 03:16:34 +0200

    Updated supabase service

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-// lib/main.dart
-// Entry point of the application. This file initializes the app and runs the root widget.
-
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: const String.fromEnvironment('SUPABASE_URL'),
+    anonKey: const String.fromEnvironment('SUPABASE_KEY'),
+  );
+
   runApp(const MyApp());
 }
